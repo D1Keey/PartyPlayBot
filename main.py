af3146dbaf0458217e6378ed116ad4537932f52a
D1Keey <devilmax66697@gmail.com>
2024-11-12 21:52:50 +0500

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import telebot
 import openai
-from config import OpenAiKey
-from config import TgKey
+from config import OpenAiKey, TgKey
 
 openai.api_key = OpenAiKey
-bot = telebot.Telebot(TgKey)
+bot = telebot.TeleBot(TgKey)  # исправлено Telebot на TeleBot
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    bot.send_message(mesaage.chat.id, 'Привет')
+    bot.send_message(message.chat.id, 'Привет')  # исправлено mesaage на message
 
 @bot.message_handler(content_types=["text"])
 def talk(message):
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model="gpt-3.5-turbo",
         prompt=message.text,
         temperature=0.5,
         max_tokens=1000,
